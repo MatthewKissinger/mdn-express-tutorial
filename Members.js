01d69c76421a251b0fda00386a337a4c36f9fc22
Matt <matthew.t.kissinger@gmail.com>
2023-07-27 11:02:54 -0700

    using url parameters

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id: 1, 
+        name: "John Doe", 
+        email: "jdoe@gmail.com",
+        status: "active"
+    },
+    {
+        id: 2, 
+        name: "Shannon Jackson",
+        email: "sjackson@gmail.com",
+        status: "active"
+    }
+]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const path = require('path');
+
+const logger = require('./middleware/logger');
+const members = require('./Members');
+
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// Init middleware
+// app.use(logger);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+// Gets All Members
+app.get("/api/members", (req, res) => {
+    res.json(members);
+})
+
+// Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json( { msg: `No member with the id of ${req.params.id}` });
+    }
+})
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}!`);
 });
